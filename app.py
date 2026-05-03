016834c4081668b5997bb7540459006077a665eb
Arief Shah <ariefdanielshah@gmail.com>
2026-05-03 19:44:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Streamlit App", layout="wide")
+
+st.title("🏠 Main Page")
+st.write("Welcome to your Streamlit multi-page app!")
+
+st.sidebar.success("Select a page above 👆")
